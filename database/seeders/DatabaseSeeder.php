a4d21175f432242305d831d1118a757c3817b0ef
Igoris Ivanovas <igoris.x@inbox.lt>
2021-09-21 15:37:51 +0300

    seeder done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,14 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
+
 {
+    
     /**
      * Seed the application's database.
      *
@@ -13,6 +18,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $faker = Faker::create('it_IT');
+       DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+
+        $horsesCount = 20;
+        foreach(range(1, $horsesCount) as $_) {
+            $runs = rand(0,25);
+            DB::table('horses')->insert([
+                'name' => $faker->firstName(),
+                'runs' => $runs,
+                'wins' => rand(0,$runs),
+                'about' => $faker->text($maxNbChars = 200),
+            ]);
+        }
+
+      foreach(range(1, 50) as $_) {
+            DB::table('betters')->insert([
+                'name' => $faker->firstName(),
+                'surname' => $faker->lastName(),
+                'bet' => rand(20, 5000),
+                'horse_id' => rand(1,$horsesCount),
+
+            ]);
+        }
     }
 }
